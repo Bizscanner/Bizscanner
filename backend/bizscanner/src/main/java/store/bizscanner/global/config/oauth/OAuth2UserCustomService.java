3d297bc072985ffb211969d85c08548babf91daf
구기현 <rnrl4611@gmail.com>
2023-09-22 11:38:58 +0900

    fix: provider 정보 추가

--- backend/bizscanner/src/main/java/store/bizscanner/global/config/oauth/OAuth2UserCustomService.java
+++ backend/bizscanner/src/main/java/store/bizscanner/global/config/oauth/OAuth2UserCustomService.java
@@ -6,8 +6,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import store.bizscanner.entity.User;
-import store.bizscanner.repository.UserRepository;
+import store.bizscanner.entity.Member;
+import store.bizscanner.repository.MemberRepository;
 
 import java.util.Map;
 
@@ -15,28 +15,30 @@ import java.util.Map;
 @Service
 public class OAuth2UserCustomService extends DefaultOAuth2UserService {
 
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         // 요청을 바탕으로 유저 정보를 담은 객체 반환
         OAuth2User user = super.loadUser(userRequest);
-        saveOrUpdate(user);
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        saveOrUpdate(user, provider);
         return user;
     }
 
     // 유저가 있으면 업데이트, 없으면 유저 생성
-    private User saveOrUpdate(OAuth2User oAuth2User) {
+    private Member saveOrUpdate(OAuth2User oAuth2User, String provider) {
         Map<String, Object> attributes = oAuth2User.getAttributes();
         String email = (String) attributes.get("email");
         String name = (String) attributes.get("name");
-        User user = userRepository.findByEmail(email)
+        Member member = memberRepository.findByEmail(email)
                 .map(entity -> entity.update(name))
-                .orElse(User.builder()
+                .orElse(Member.builder()
                         .email(email)
                         .nickname(name)
+                        .provider(provider)
                         .build());
-        return userRepository.save(user);
+        return memberRepository.save(member);
     }
 
 }
